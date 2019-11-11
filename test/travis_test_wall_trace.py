efdab31ba50eda073fa0f580a9785c56c74f1a72
Naoya Nakama <naoya.nakama@gmail.com>
2019-11-12 00:49:07 +0900

    trace test fail

--- test/travis_test_wall_trace.py
+++ test/travis_test_wall_trace.py
@@ -22,7 +22,7 @@ class WallTraceTest(unittest.TestCase):
         self.assertTrue(left == right == 0, "can't stop")
 
         left, right = self.set_and_get(0,5,1000,0) #side direction is not a trigger of stop
-        self.assertTrue(left == right != 0, "stop wrongly by side sensors")
+        self.assertTrue(left != 0 and right != 0, "stop wrongly by side sensors")
 
         left, right = self.set_and_get(0,10,0,0) #curve to right
         self.assertTrue(left < right, "don't cyrve to left")
